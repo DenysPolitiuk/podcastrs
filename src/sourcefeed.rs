fa0c190b3e83267446d55946eebe7f0cc1eee85f
DenysPolitiuk <denys.politiuk@protonmail.com>
2019-08-06 18:44:27 -0400

    Added new to SourceFeed

--- src/sourcefeed.rs
+++ src/sourcefeed.rs
@@ -1,3 +1,11 @@
 pub struct SourceFeed {
     pub url: String,
 }
+
+impl SourceFeed {
+    pub fn new(url: &str) -> SourceFeed {
+        SourceFeed {
+            url: url.to_string(),
+        }
+    }
+}
