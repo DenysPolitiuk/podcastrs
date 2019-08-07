321863f08d02dfa2be7bd9ae58064b74895adcd5
DenysPolitiuk <denys.politiuk@protonmail.com>
2019-08-06 20:17:24 -0400

    Added add new feed and restructured other tests

--- src/rssscheduler.rs
+++ src/rssscheduler.rs
@@ -31,35 +31,73 @@ impl RssScheduler {
     pub fn get_source_feed(&self, url: &str) -> Option<&SourceFeed> {
         self.source_feeds.get(url)
     }
+
+    pub fn add_new_feed(&mut self, new_feed: RssFeed) {
+        let source_url = new_feed.get_source_feed();
+        match self.rss_feeds.entry(source_url.clone()) {
+            Entry::Occupied(mut v) => {
+                let feeds = v.get_mut();
+                feeds.push(new_feed);
+            }
+            Entry::Vacant(v) => {
+                v.insert(vec![new_feed]);
+            }
+        };
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    const SOURCE1: &str = "test1";
+    const SOURCE2: &str = "test2";
+    const SOURCE3: &str = "test3";
+    const INVALID_SOURCE: &str = "invalid";
+
+    const FEED1_FILE: &str = "tests/sedaily.rss";
+    const FEED2_FILE: &str = "tests/hn.rss";
+
+    fn set_up_scheduler() -> RssScheduler {
+        let mut scheduler = RssScheduler::new();
+        scheduler.add_source_feed(SOURCE1);
+        scheduler.add_source_feed(SOURCE2);
+        scheduler.add_source_feed(SOURCE3);
+
+        scheduler
+    }
+
+    fn set_up_scheduler_with_feeds() -> RssScheduler {
+        let mut scheduler = set_up_scheduler();
+
+        let feed1 = RssFeed::new_from_file(SOURCE1, FEED1_FILE).unwrap();
+        let feed2 = RssFeed::new_from_file(SOURCE2, FEED2_FILE).unwrap();
+        let feed3 = RssFeed::new_from_file(SOURCE2, FEED1_FILE).unwrap();
+        let feed4 = RssFeed::new_from_file(SOURCE1, FEED2_FILE).unwrap();
+
+        scheduler.add_new_feed(feed1);
+        scheduler.add_new_feed(feed2);
+        scheduler.add_new_feed(feed3);
+        scheduler.add_new_feed(feed4);
+
+        scheduler
+    }
+
     #[test]
     fn add_source_feed() {
         let mut scheduler = RssScheduler::new();
 
         assert_eq!(scheduler.source_feeds.len(), 0);
-        assert_eq!(scheduler.add_source_feed("test1"), true);
-        assert_eq!(scheduler.add_source_feed("test2"), true);
-        assert_eq!(scheduler.add_source_feed("test2"), false);
-        assert_eq!(scheduler.add_source_feed("test3"), true);
+        assert_eq!(scheduler.add_source_feed(SOURCE1), true);
+        assert_eq!(scheduler.add_source_feed(SOURCE2), true);
+        assert_eq!(scheduler.add_source_feed(SOURCE2), false);
+        assert_eq!(scheduler.add_source_feed(SOURCE3), true);
         assert_eq!(scheduler.source_feeds.len(), 3);
     }
 
     #[test]
     fn get_source_feeds() {
-        const SOURCE1: &str = "test1";
-        const SOURCE2: &str = "test2";
-        const SOURCE3: &str = "test3";
-        const INVALID_SOURCE: &str = "invalid";
-
-        let mut scheduler = RssScheduler::new();
-        scheduler.add_source_feed(SOURCE1);
-        scheduler.add_source_feed(SOURCE2);
-        scheduler.add_source_feed(SOURCE3);
+        let scheduler = set_up_scheduler();
 
         assert_eq!(scheduler.get_source_feed(SOURCE1).unwrap().url, SOURCE1);
         assert_eq!(scheduler.get_source_feed(SOURCE2).unwrap().url, SOURCE2);
@@ -68,12 +106,21 @@ mod tests {
     }
 
     #[test]
+    fn add_new_feed() {
+        let scheduler = set_up_scheduler_with_feeds();
+
+        assert_eq!(scheduler.rss_feeds.len(), 2);
+        assert_eq!(scheduler.rss_feeds.get(SOURCE1).unwrap().len(), 2);
+        assert_eq!(scheduler.rss_feeds.get(SOURCE2).unwrap().len(), 2);
+    }
+
+    #[test]
     fn get_new_feed_from_feed_source() {
         panic!("unimplemented");
     }
 
     #[test]
-    fn check_feed_update() {
+    fn check_feed_need_update() {
         panic!("unimplemented");
     }
 
