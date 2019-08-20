e125c025cdae33d21ae2ecf83fd30b95c3744806
DenysPolitiuk <denys.politiuk@protonmail.com>
2019-08-19 22:58:16 -0400

    Added tags to allow deletion of old droplets

--- deploy-scripts/docker-create-podrocket-droplet.sh
+++ deploy-scripts/docker-create-podrocket-droplet.sh
@@ -1,4 +1,5 @@
-docker-machine create --driver=digitalocean --digitalocean-access-token=$DO_TOKEN --digitalocean-size=s-1vcpu-1gb podrocket
+curl -X DELETE -H "Content-Type: application/json" -H "Authorization: Bearer $DO_TOKEN" "https://api.digitalocean.com/v2/droplets?tag_name=podcastrs-podrocket"
+docker-machine create --driver=digitalocean --digitalocean-access-token=$DO_TOKEN --digitalocean-size=s-1vcpu-1gb --digitalocean-tags podcastrs-podrocket podrocket
 eval "$(docker-machine env podrocket)"
 cd docker/podrocket
 docker-compose up --build -d
