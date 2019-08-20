a1192c5dd951b7819acd68321a1b04dbe76d98a0
DenysPolitiuk <denys.politiuk@protonmail.com>
2019-08-19 22:02:42 -0400

    Small change in create script

--- deploy-scripts/docker-create-podrocket-droplet.sh
+++ deploy-scripts/docker-create-podrocket-droplet.sh
@@ -1,4 +1,4 @@
 docker-machine create --driver=digitalocean --digitalocean-access-token=$DO_TOKEN --digitalocean-size=s-1vcpu-1gb podrocket
-eval $(docker-machine env podrocket)
+eval "$(docker-machine env podrocket)"
 cd docker/podrocket
 docker-compose up --build -d
